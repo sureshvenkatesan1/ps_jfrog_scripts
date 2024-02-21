36384b0c4d37bf2ad7a91688af2467150c852ba1
Suresh Venkatesan <sureshv@jfrog.com>
2024-02-20 22:43:04 -0800

    AU-0 - Updated patch_props_for_artifacts_in_target.sh

--- jf-transfer-migration-helper-scripts/patch_props_for_artifacts_already_in_target/patch_props_for_artifacts_in_target.sh
+++ jf-transfer-migration-helper-scripts/patch_props_for_artifacts_already_in_target/patch_props_for_artifacts_in_target.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# ./patch_props_for_artifacts_in_target.sh usvartifactory5 liquid jfrogio liquid  test 
+# ./patch_props_for_artifacts_in_target.sh usvartifactory5 liquid jfrogio liquid  test
 
 # Check if at least the first four required parameters are provided
 if [ $# -lt 4 ]; then
-    echo "Usage: $0 <source-artifactory> <source-repo> <target-repo> <target-artifactory>  [root-folder]"
+    echo "Usage: $0 <source-artifactory> <source-repo> <target-artifactory> <target-repo> [root-folder]"
     exit 1
 fi
 
@@ -20,77 +20,201 @@ failed_commands_file="failed_commands.txt"
 all_commands_file="all_commands.txt"
 # skipped_commands_file="skipped_commands.txt"
 
-generate_props_patch_command() {
-   #  generate_props_patch_command  "$target_list_in_subfolders_command" "$folder_to_migrate" "$((folder_position+1))" "$total_folders"
+#####################################
+# Can be deleted later.
+# Function to generate props patch command
+# generate_props_patch_command1() {
+#    #  generate_props_patch_command  "$target_list_in_subfolders_command" "$folder_to_migrate" "$((folder_position+1))" "$total_folders"
 
-    target_list_command="$1"
-    folder_to_migrate="$2"
-    folder_position="$3"  # Pass the folder position as an argument
-    sibling_folder_count="$4"
+#     target_list_command="$1"
+#     folder_to_migrate="$2"
+#     folder_position="$3"  # Pass the folder position as an argument
+#     sibling_folder_count="$4"
 
 
-    eval "$target_list_command  >> a"
-    target_exit_status=$?
-   
-    if [ $target_exit_status -ne 0 ]; then
-        echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $target_list_command"
-        echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $target_list_command" >> "$failed_commands_file"
+#     eval "$target_list_command  >> a"
+#     target_exit_status=$?
+
+#     if [ $target_exit_status -ne 0 ]; then
+#         echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $target_list_command"
+#         echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $target_list_command" >> "$failed_commands_file"
+#     fi
+#     # Disable debugging when no longer needed
+# #  set +x
+#     if  [ $target_exit_status -eq 0 ]; then
+#           cat a |  sed -re 's/,[[:alnum:]]+"$/"/g' |sed 's/"//g' > b
+#         #   cat b
+
+#            while IFS= read -r line
+#             do
+
+#                 # Does the artifact have properties
+#                 get_item_properties_cmd="jf rt  curl -s -k -XGET \"/api/storage/$source_repo/$line?properties\" --server-id $source_artifactory"
+#                 # echo $get_item_properties_cmd
+#                 prop_output=$(eval "$get_item_properties_cmd")
+#                 prop_exit_status=$?
+
+
+#                 if [ $prop_exit_status -ne 0 ]; then
+#                     echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $get_item_properties_cmd"
+#                     echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $get_item_properties_cmd" >> "$failed_commands_file"
+#                 else
+#                     # echo $prop_output
+#                     # Check the status code and process the JSON data
+#                     http_status=$(echo "$prop_output" | jq -r '.errors[0].status')
+#                     if [ "$http_status" != "404" ]; then
+#                        # The artifact has properties
+#                         json_data=$(echo "$prop_output" | jq -c '.properties')
+#                         # Check if the JSON data is not null before further processing
+#                         if [ -n "$json_data" ]; then
+
+#                             modified_json="{\"props\": $json_data}"
+#                             escaped_modified_json=$(echo "$modified_json" | sed 's/"/\\"/g')
+#                             target_props_output=$(jf rt curl -k -sL -XGET "/api/storage/$target_repo/$line?properties" --server-id $target_artifactory)
+#                             # echo "*********>$target_props_output"
+#                             if [ -n "$target_props_output" ]; then
+#                                 target_json_data=$(echo "$target_props_output" | jq -c '.properties')
+#                                 if [ -n "$target_json_data" ]; then
+#                                     target_modified_json="{\"props\": $target_json_data}"
+#                                     target_escaped_modified_json=$(echo "$target_modified_json" | sed 's/"/\\"/g')
+#                                 fi
+#                             fi
+
+#                             # echo "~~~~~~~~~~>$target_escaped_modified_json"
+
+#                             if [ "$target_escaped_modified_json" == "$escaped_modified_json" ]; then
+#                                 echo "Properties are identical, skipping patching for artifact: $line"
+#                             else
+#                                 # Run the PATCH request using the modified JSON data to set the properties for the artifact in the target
+#                                 # ( artifact already exists in the target)
+
+#                                 echo " jf rt curl -k -sL -XPATCH -H \"Content-Type: application/json\" \"/api/metadata/$target_repo/$line?atomicProperties=1\" --server-id $target_artifactory -d \"$escaped_modified_json\" "$'\n'
+#                             fi
+#                         else
+#                             echo "No properties found for artifact: $line"
+#                         fi
+
+#                    fi
+
+#                 fi
+
+#             done < "b"
+
+#         rm -f a b
+#     fi
+# }
+#####################################
+
+####################
+# Function to execute a command and log failures
+execute_command() {
+    local command="$1"
+    local folder="$2"
+
+    eval "$command >> a"
+    local exit_status=$?
+
+    if [ $exit_status -ne 0 ]; then
+        echo "Error: Command failed for folder: $folder - Run Command: $command"
+        echo "Error: Command failed for folder: $folder - Run Command: $command" >> "$failed_commands_file"
     fi
-    # Disable debugging when no longer needed
-#  set +x
-    if  [ $target_exit_status -eq 0 ]; then
-          cat a |  sed -re 's/,[[:alnum:]]+"$/"/g' |sed 's/"//g' > b
-        #   cat b
 
-           while IFS= read -r line
-            do
+    return $exit_status
+}
 
-                # Does the artifact have properties
-                get_item_properties_cmd="jf rt  curl -s -k -XGET \"/api/storage/$source_repo/$line?properties\" --server-id $source_artifactory"
-                # echo $get_item_properties_cmd
-                prop_output=$(eval "$get_item_properties_cmd")
-                prop_exit_status=$?
+# Function to process properties for an artifact
+process_artifact_properties() {
+    local line="$1"
+    local folder="$2"
+    local artifactory_id="$3"
+    local repo="$4"
+
+    # Check if the artifact has properties
+    local get_item_properties_cmd="jf rt  curl -s -k -XGET \"/api/storage/$repo/$line?properties\" --server-id $artifactory_id"
+    local prop_output=$(eval "$get_item_properties_cmd")
+    local prop_exit_status=$?
+
+    if [ $prop_exit_status -ne 0 ]; then
+        echo "Error: Command failed for folder: $folder - Run Command: $get_item_properties_cmd"
+        echo "Error: Command failed for folder: $folder - Run Command: $get_item_properties_cmd" >> "$failed_commands_file"
+        return 1
+    fi
+
+    # Check if the artifact has properties
+    local http_status=$(echo "$prop_output" | jq -r '.errors[0].status')
+    if [ "$http_status" != "404" ]; then
+        local json_data=$(echo "$prop_output" | jq -c '.properties')
+        if [ -n "$json_data" ]; then
+            echo "$json_data"
+            return 0
+        fi
+    fi
+
+    return 1
+}
+
+# Function to generate props patch command for an artifact
+generate_patch_command() {
+    local line="$1"
+    local folder="$2"
+    local escaped_modified_json="$3"
+
+    local target_json_data=$(process_artifact_properties "$line" "$folder_to_migrate" "$target_artifactory" "$target_repo")
 
 
-                if [ $prop_exit_status -ne 0 ]; then
-                    echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $get_item_properties_cmd"
-                    echo "Error: Command failed for folder: $folder_to_migrate - Run Command: $get_item_properties_cmd" >> "$failed_commands_file"
-                else
-                    # echo $prop_output
-                    # Check the status code and process the JSON data
-                    http_status=$(echo "$prop_output" | jq -r '.errors[0].status')
-                    if [ "$http_status" != "404" ]; then
-                       # The artifact has properties
-                        json_data=$(echo "$prop_output" | jq -c '.properties')
+    if [ $? -eq 0 ] && [ -n "$target_json_data" ]; then
+        local target_modified_json="{\"props\": $target_json_data}"
+        local target_escaped_modified_json=$(echo "$target_modified_json" | sed 's/"/\\"/g')
 
-                         modified_json="{\"props\": $json_data}"
-                         escaped_modified_json=$(echo "$modified_json" | sed 's/"/\\"/g')
-                        # Run the PATCH request using the modified JSON data to set the properties for the artifact in the target 
-                        # ( artifact already exists in the target)
+        if [ "$target_escaped_modified_json" == "$escaped_modified_json" ]; then
+            echo "Properties are identical, skipping patching for artifact: $line"
+        else
+            echo "jf rt curl -k -sL -XPATCH -H \"Content-Type: application/json\" \"/api/metadata/$target_repo/$line?atomicProperties=1\" --server-id $target_artifactory -d \"$escaped_modified_json\" "
+        fi
+    fi
 
-                        echo " jf rt curl -k -sL -XPATCH -H \"Content-Type: application/json\" \"/api/metadata/$target_repo/$line?atomicProperties=1\" --server-id $target_artifactory -d \"$escaped_modified_json\" "$'\n' 
- 
-                   fi
 
-                fi
+}
 
-            done < "b"
-        
+# Function to generate props patch command
+generate_props_patch_command() {
+    local target_list_command="$1"
+    local folder_to_migrate="$2"
+    local folder_position="$3"
+    local sibling_folder_count="$4"
+
+    execute_command "$target_list_command" "$folder_to_migrate"
+    local exit_status=$?
+
+    if [ $exit_status -eq 0 ]; then
+        cat a | sed -re 's/,[[:alnum:]]+"$/"/g' | sed 's/"//g' > b
+
+        while IFS= read -r line; do
+            local json_data=$(process_artifact_properties "$line" "$folder_to_migrate" "$source_artifactory" "$source_repo")
+            if [ $? -eq 0 ] && [ -n "$json_data" ]; then
+                local modified_json="{\"props\": $json_data}"
+                local escaped_modified_json=$(echo "$modified_json" | sed 's/"/\\"/g')
+                generate_patch_command "$line" "$folder_to_migrate" "$escaped_modified_json"
+            else
+                echo "No properties found for artifact: $line"
+            fi
+        done < "b"
+
         rm -f a b
     fi
 }
-
+######################
 # Check if the fifth parameter (root-folder) is provided
 if [ $# -ge 5 ]; then
     root_folder="$5"
 
-    
+
 
     target_command1="jf rt curl -s -XPOST -H 'Content-Type: text/plain' api/search/aql --server-id $target_artifactory --insecure \
     --data 'items.find({\"repo\":  {\"\$eq\":\"$target_repo\"}, \"path\": {\"\$match\": \"$root_folder\"},\
         \"type\": \"file\"}).include(\"repo\",\"path\",\"name\",\"sha256\")'"
 
-        # Concatenate the two commands 
+        # Concatenate the two commands
     target_list_in_dot_folder_command="$target_command1 $jq_sed_command"
 
 
@@ -99,12 +223,12 @@ if [ $# -ge 5 ]; then
 else
     root_folder=""
     # check for files in the root folder:
-    
+
     target_command1="jf rt curl -s -XPOST -H 'Content-Type: text/plain' api/search/aql --server-id $target_artifactory --insecure \
     --data 'items.find({\"repo\":  {\"\$eq\":\"$target_repo\"}, \"path\": {\"\$match\": \".\"},\
         \"type\": \"file\"}).include(\"repo\",\"path\",\"name\",\"sha256\")'"
 
-        # Concatenate the two commands 
+        # Concatenate the two commands
     target_list_in_dot_folder_command="$target_command1 $jq_sed_command"
 
     #Call the migrate command without the trailing * in $folder_to_migrate
@@ -114,9 +238,9 @@ fi
 
 # Run the jf rt curl command and capture the output into a variable
 if [ -z "$root_folder" ]; then
-    output=$(jf rt curl -s -k -XGET "/api/storage/$source_repo?list&deep=1&depth=1&listFolders=1" --server-id $target_artifactory)
+    output=$(jf rt curl -s -k -XGET "/api/storage/$target_repo?list&deep=1&depth=1&listFolders=1" --server-id $target_artifactory)
 else
-    output=$(jf rt curl -s -k -XGET "/api/storage/$source_repo/$root_folder?list&deep=1&depth=1&listFolders=1" --server-id $target_artifactory)
+    output=$(jf rt curl -s -k -XGET "/api/storage/$target_repo/$root_folder?list&deep=1&depth=1&listFolders=1" --server-id $target_artifactory)
 fi
 
 # Parse the JSON output using jq and get the "uri" values for folders
@@ -137,7 +261,7 @@ total_folders="$(expr "${#folders_array[@]}" + 1)"
         #Remove the leading slash i.e if folder is "/abc" it becomes "abc"
         folder="${folder#/}"
         # Check if the folder name is ".conan" and skip it as it will be generated
-        if [ "$folder" = ".conan" ]; then
+        if [ "$folder" = ".conan" ] ||  [ "$folder" = ".jfrog" ]; then
             continue  # Skip this iteration of the loop
         fi
 
@@ -151,15 +275,15 @@ total_folders="$(expr "${#folders_array[@]}" + 1)"
         else
             folder_to_migrate="$root_folder/$folder"
         fi
-        
+
 
         target_command1="jf rt curl -s -XPOST -H 'Content-Type: text/plain' api/search/aql --server-id $target_artifactory --insecure \
         --data 'items.find({\"repo\":  {\"\$eq\":\"$target_repo\"}, \"path\": {\"\$match\": \"$folder_to_migrate\"},\
             \"type\": \"file\"}).include(\"repo\",\"path\",\"name\",\"sha256\")'"
 
-            # Concatenate the two commands 
+            # Concatenate the two commands
         target_list_in_this_folder_command="$target_command1 $jq_sed_command"
-        
+
 
     #    echo $target_list_command
 
@@ -169,13 +293,13 @@ total_folders="$(expr "${#folders_array[@]}" + 1)"
 
     # Now check in the subfolders:
 
-        
+
 
         target_command2="jf rt curl -s -XPOST -H 'Content-Type: text/plain' api/search/aql --server-id $target_artifactory --insecure \
         --data 'items.find({\"repo\":  {\"\$eq\":\"$target_repo\"}, \"path\": {\"\$match\": \"$folder_to_migrate/*\"},\
             \"type\": \"file\"}).include(\"repo\",\"path\",\"name\",\"sha256\")'"
 
-        # Concatenate the two commands 
+        # Concatenate the two commands
 
         target_list_in_subfolders_command="$target_command2 $jq_sed_command"
         # echo $target_list_in_subfolders_command
@@ -189,4 +313,3 @@ total_folders="$(expr "${#folders_array[@]}" + 1)"
         #     wait -n
         # fi
     done #| parallel -j 8
-
--- jf-transfer-migration-helper-scripts/patch_props_for_artifacts_already_in_target/readme.md
+++ jf-transfer-migration-helper-scripts/patch_props_for_artifacts_already_in_target/readme.md
@@ -1,39 +1,45 @@
 # Bash Script for Patching Properties of Artifacts in Target Artifactory
 
-This Bash script is designed to patch the properties of artifacts in a target Artifactory repository based on the properties of corresponding artifacts in a source Artifactory repository.
+This Bash script is designed to patch the properties of artifacts in a target Artifactory repository based on the properties of corresponding artifacts in a source Artifactory repository ( if they are different)
+
+### Prerequisites
+
+- **bash**: Ensure you have Bash installed on your system.
+- **jq**: This script uses `jq` to parse JSON responses. Make sure `jq` is installed and accessible in your environment.
+- **JFrog CLI**: The script relies on `jf` commands for interacting with Artifactory repositories. Make sure you have JFrog CLI installed and configured.
 
 ## Usage
-
-```bash
-./patch_props_for_artifacts_in_target.sh <source-artifactory> <source-repo> <target-repo> <target-artifactory> [root-folder]
+To generate the jq commands:
+```
+bash ./patch_props_for_artifacts_in_target.sh <source-artifactory> <source-repo> <target-artifactory> <target-repo>  [root-folder]
+```
+To generate and execute the jq commands using [runcommand_in_parallel_and_log_outcome.sh](../runcommand_in_parallel_as_bash_jobs/runcommand_in_parallel_and_log_outcome.sh):
+```
+bash ./patch_props_for_artifacts_in_target.sh <source-artifactory> <source-repo> <target-artifactory> <target-repo>  [root-folder] | bash ./runcommand_in_parallel_and_log_outcome.sh properties_patch_failed.txt 16
 ```
 
-- <source-artifactory>: The source Artifactory server.
-- <source-repo>: The source repository within the source Artifactory server.
-- <target-repo>: The target repository within the target Artifactory server.
-- <target-artifactory>: The target Artifactory server.
-- [root-folder]: (Optional) The root folder in the source repository to start processing. If not provided, the script will process artifacts in the root of the source repository.
+- `source-artifactory`: The source Artifactory server.
+- `source-repo`: The source repository within the source Artifactory server.
+- `target-artifactory`: The target Artifactory server.
+- `target-repo`: The target repository within the target Artifactory server.
+- `[root-folder]`: (Optional) The root folder in the source repository to start processing. If not provided, the script will process artifacts from the root of the source repository.
 
 ## Script Overview
 The script performs the following steps:
 
-- Check if the required parameters are provided.
-- Generate a command to list artifacts in the source repository and their properties.
-- Process each artifact:
-    - Check if the artifact has properties in the source repository.
-    - If properties exist, patch the properties for the corresponding artifact in the target repository.
-- List all folders in the source repository.
-- Process artifacts in each folder recursively, updating properties in the target repository.
-- Limit parallel execution of commands (optional, currently commented out).
-Please refer to the script code for detailed comments and explanations of each step.
+- The script retrieves artifacts from the target repository and their properties using JFrog CLI.
+- It compares the properties of these artifacts with their counterparts in the source repository.
+- If differences are found, it patches the properties of the target artifacts to match those of the source artifacts.
+- The script logs failed commands to `failed_commands.txt`.
 
 ## Notes
 The script uses jq for JSON parsing and sed for string manipulation.
 Failed commands are logged to `failed_commands.txt``.
-Successful and all commands are logged to `all_commands.txt``.
 
 
-This script generates tons of following commands to PATCH the properties of each artifact:
+
+This script generates tons of following commands to PATCH the properties of each artifact with the property differs 
+in source and target artifactory instances:
 ```
 " jf rt curl -k -sL -XPATCH -H \"Content-Type: application/json\" \"/api/metadata/$target_repo/$line?atomicProperties=1\" --server-id $target_artifactory -d \"$escaped_modified_json\" "
 ```
@@ -47,6 +53,37 @@ For example to run 16 such commands at a time run  using
 ./patch_props_for_artifacts_in_target.sh usvartifactory5 liquid jfrogio liquid  test | ./runcommand_in_parallel_and_log_outcome.sh properties_patch_failed.txt 16
 
 ```
+
+Other way to run this:
+```
+bash ./patch_props_for_artifacts_already_in_target/patch_props_for_artifacts_in_target.sh \
+soleng app1-docker-dev-local proservicesone test-docker
+
+```
+Output will be similar to the following:
+```
+jf rt curl -k -sL -XPATCH -H "Content-Type: application/json" "/api/metadata/test-docker/hello-world/5.7/manifest.json?atomicProperties=1" --server-id proservicesone -d "{\"props\": {\"artifactory.content-type\":[\"application/vnd.docker.distribution.manifest.v2+json\"],\"docker.manifest\":[\"5.7\"],\"docker.manifest.digest\":[\"sha256:01433e86a06b752f228e3c17394169a5e21a0995f153268a9b36a16d4f2b2184\"],\"docker.manifest.type\":[\"application/vnd.docker.distribution.manifest.v2+json\"],\"docker.repoName\":[\"hello-world\"],\"sha256\":[\"01433e86a06b752f228e3c17394169a5e21a0995f153268a9b36a16d4f2b2184\"]}}"
+Properties are identical, skipping patching for artifact: hello-world/5.7/sha256__18e5af7904737ba5ef7fbbd7d59de5ebe6c4437907bd7fc436bf9b3ef3149ea9
+Properties are identical, skipping patching for artifact: hello-world/5.7/sha256__93288797bd35d114f2d788e5abf4fae518a5bd299647daf4ede47acc029d66c5
+```
+
+You can even run it for a subfolder as follows:
+```
+bash ./patch_props_for_artifacts_already_in_target/patch_props_for_artifacts_in_target.sh \
+soleng app1-docker-dev-local proservicesone test-docker hello-world/5.7
+```
+Then these "jf" commands in the output can be executed as:
+```
+bash /tmp/ps_jfrog_scripts/jf-transfer-migration-helper-scripts/patch_props_for_artifacts_already_in_target/patch_props_for_artifacts_in_target.sh \
+soleng app1-docker-dev-local proservicesone test-docker hello-world/5.7 | bash /tmp/ps_jfrog_scripts/jf-transfer-migration-helper-scripts/runcommand_in_parallel_as_bash_jobs/runcommand_in_parallel_and_log_outcome.sh properties_patch_failed.txt 16
+```
+In the output you should see output similar to:
+```
+Command successful: jf rt curl -k -sL -XPATCH -H "Content-Type: application/json" "/api/metadata/test-docker/hello-world/5.7/manifest.json?atomicProperties=1" --server-id proservicesone -d "{\"props\": {\"artifactory.content-type\":[\"application/vnd.docker.distribution.manifest.v2+json\"],\"docker.manifest\":[\"5.7\"],\"docker.manifest.digest\":[\"sha256:01433e86a06b752f228e3c17394169a5e21a0995f153268a9b36a16d4f2b2184\"],\"docker.manifest.type\":[\"application/vnd.docker.distribution.manifest.v2+json\"],\"docker.repoName\":[\"hello-world\"],\"sha256\":[\"01433e86a06b752f228e3c17394169a5e21a0995f153268a9b36a16d4f2b2184\"]}}"
+Command successful: Properties are identical, skipping patching for artifact: hello-world/5.7/sha256__18e5af7904737ba5ef7fbbd7d59de5ebe6c4437907bd7fc436bf9b3ef3149ea9
+Command successful: Properties are identical, skipping patching for artifact: hello-world/5.7/sha256__93288797bd35d114f2d788e5abf4fae518a5bd299647daf4ede47acc029d66c5
+```
+---
 
 I prefer the above command insted of the followig whcih uses the bash xargs approach to run 16 such commands at a time using [runcommand_w_xargs_and_log_outcome.sh](../runcommand_in_parallel_as_bash_jobs/runcommand_w_xargs_and_log_outcome.sh):
 ```
